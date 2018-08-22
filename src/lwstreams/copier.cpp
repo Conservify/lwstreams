25acfdc1ff59f0199d7121a9d4a998875af65e6b
Jacob Lewallen <jlewallen@gmail.com>
2018-08-22 13:07:34 -0700

    Methods for getting position/available information.

--- src/lwstreams/copier.cpp
+++ src/lwstreams/copier.cpp
@@ -3,43 +3,43 @@
 namespace lws {
 
 void StreamCopier::restart() {
-    position = 0;
+    position_ = 0;
 }
 
 int32_t StreamCopier::copy(Reader &reader, Writer &writer) {
-    auto available = buffer.size - position;
+    auto available = buffer_.size - position_;
     auto eos = false;
     auto copied = 0;
 
     if (available > 0) {
-        auto read = reader.read(buffer.ptr + position, available);
+        auto read = reader.read(buffer_.ptr + position_, available);
         if (read == Stream::EOS) {
             eos = true;
         }
         else if (read > 0) {
-            position += read;
+            position_ += read;
         }
     }
 
-    if (position > 0) {
-        auto wrote = writer.write(buffer.ptr, position);
+    if (position_ > 0) {
+        auto wrote = writer.write(buffer_.ptr, position_);
         if (wrote == Stream::EOS) {
             return Stream::EOS;
         }
         if (wrote > 0) {
             copied = wrote;
-            if (wrote != (int32_t)position) {
-                auto remaining = position - wrote;
-                memmove(buffer.ptr, buffer.ptr + wrote, remaining);
-                position = remaining;
+            if (wrote != (int32_t)position_) {
+                auto remaining = position_ - wrote;
+                memmove(buffer_.ptr, buffer_.ptr + wrote, remaining);
+                position_ = remaining;
             }
             else {
-                position = 0;
+                position_ = 0;
             }
         }
     }
 
-    if (copied == 0 && position == 0 && eos) {
+    if (copied == 0 && position_ == 0 && eos) {
         return Stream::EOS;
     }
 
--- src/lwstreams/copier.h
+++ src/lwstreams/copier.h
@@ -7,11 +7,11 @@ namespace lws {
 
 class StreamCopier {
 private:
-    BufferPtr buffer;
-    size_t position{ 0 };
+    BufferPtr buffer_;
+    size_t position_{ 0 };
 
 public:
-    StreamCopier(BufferPtr &&bp) : buffer(std::forward<BufferPtr>(bp)) {
+    StreamCopier(BufferPtr &&bp) : buffer_(std::forward<BufferPtr>(bp)) {
     }
 
 public:
@@ -19,15 +19,24 @@ public:
 
     int32_t copy(Reader &reader, Writer &writer);
 
+public:
+    size_t position() const {
+        return position_;
+    }
+
+    size_t available() const {
+        return buffer_.size - position_;
+    }
+
 };
 
 template<size_t Size>
 class BufferedStreamCopier : public StreamCopier {
 private:
-    lws::AlignedStorageBuffer<Size> buffer;
+    lws::AlignedStorageBuffer<Size> buffer_;
 
 public:
-    BufferedStreamCopier() : StreamCopier(buffer.toBufferPtr()) {
+    BufferedStreamCopier() : StreamCopier(buffer_.toBufferPtr()) {
     }
 
 };
